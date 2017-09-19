f0a0e33edad69669996e1b3223352717298c5b82
wangminghuan <m.h.wang@foxmail.com>
2017-09-19 22:31:52 +0800

    完善npm script获取逻辑

--- path.config.js
+++ path.config.js
@@ -1,21 +1,18 @@
 var path=require('path');
 var argv=null;
 try { 
-	console.log( process.env);
  argv = JSON.parse(process.env.npm_config_argv).original;
 }catch(ex){    
 	argv = process.argv;
 }
 
 if(argv[argv.length-1]=="undefined" ||argv[argv.length-1]=="" ) {
-
 	console.log("当前项目目录不存在，请确认参数！");
-
 	return false;
-
 }
 var activeProject=argv[argv.length-1].replace(/^--/g,"");
 console.log(activeProject);
+// 通过npm run start --xxx的方式将参数传递到配置文件内部
 //定义了一些文件夹的路径
 var __path={
 	ENTRY: path.resolve(__dirname, 'app/index.js'),
